25b279a180e846902cc46f556f589feaf0850e1d
Aliaksandr Rykau <Aliaksandr_Rykau@epam.com>
2020-06-16 18:09:02 +0300

    Add NumericConversions project with unit tests.

---
+++ NumericConversions/ExplicitConversion.cs
@@ -0,0 +1,28 @@
+﻿namespace NumericConversions
+{
+    public static class ExplicitConversion
+    {
+        public static int LongToInt(long longValue)
+        {
+            return (int)longValue;
+        }
+
+        // TODO #1: Add a static method here with name "FloatToInt" that gets "floatValue" parameter ("float" type) and returns the parameter value that is explicitly converted to "int" type.
+
+        // TODO #2: Add a static method here with name "DoubleToInt" that gets "doubleValue" parameter ("double" type) and returns the parameter value that is explicitly converted to "int" type.
+
+        // TODO #3: Add a static method here with name "DecimalToInt" that gets "decimalValue" parameter ("decimal" type) and returns the parameter value that is explicitly converted to "int" type.
+
+        // TODO #4: Add a static method here with name "FloatToLong" that gets "floatValue" parameter ("float" type) and returns the parameter value that is explicitly converted to "long" type.
+
+        // TODO #5: Add a static method here with name "DoubleToLong" that gets "doubleValue" parameter ("double" type) and returns the parameter value that is explicitly converted to "long" type.
+
+        // TODO #6: Add a static method here with name "DecimalToLong" that gets "decimalValue" parameter ("decimal" type) and returns the parameter value that is explicitly converted to "long" type.
+
+        // TODO #7: Add a static method here with name "ShortToByte" that gets "shortValue" parameter ("short" type) and returns the parameter value that is explicitly converted to "byte" type.
+
+        // TODO #8: Add a static method here with name "IntToByte" that gets "intValue" parameter ("int" type) and returns the parameter value that is explicitly converted to "byte" type.
+
+        // TODO #9: Add a static method here with name "IntToShort" that gets "intValue" parameter ("int" type) and returns the parameter value that is explicitly converted to "short" type.
+    }
+}
---
+++ NumericConversions/ImplicitConversion.cs
@@ -0,0 +1,26 @@
+﻿namespace NumericConversions
+{
+    public static class ImplicitConversion
+    {
+        public static long IntToLong(int intValue)
+        {
+            return intValue;
+        }
+
+        // TODO #1: Add a static method here with name "IntToFloat" that gets "intValue" parameter ("int" type) and returns the parameter value that is implicitly converted to "float" type.
+
+        // TODO #2: Add a static method here with name "IntToDouble" that gets "intValue" parameter ("int" type) and returns the parameter value that is implicitly converted to "double" type.
+
+        // TODO #3: Add a static method here with name "IntToDecimal" that gets "intValue" parameter ("int" type) and returns the parameter value that is implicitly converted to "decimal" type.
+
+        // TODO #4: Add a static method here with name "LongToFloat" that gets "longValue" parameter ("long" type) and returns the parameter value that is implicitly converted to "float" type.
+
+        // TODO #5: Add a static method here with name "LongToDouble" that gets "longValue" parameter ("long" type) and returns the parameter value that is implicitly converted to "double" type.
+
+        // TODO #6: Add a static method here with name "LongToDecimal" that gets "longValue" parameter ("long" type) and returns the parameter value that is implicitly converted to "decimal" type.
+
+        // TODO #7: Add a static method here with name "ByteToShort" that gets "byteValue" parameter ("byte" type) and returns the parameter value that is implicitly converted to "short" type.
+
+        // TODO #8: Add a static method here with name "ByteToInt" that gets "byteValue" parameter ("byte" type) and returns the parameter value that is implicitly converted to "int" type.
+    }
+}
