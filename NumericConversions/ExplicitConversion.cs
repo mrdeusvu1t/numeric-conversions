388e2de6c42822678033a2dfd91637a415464e51
Yury Hryshanau <mr.deusvult@gmail.com>
2020-11-10 22:24:22 +0300

    Update

--- NumericConversions/ExplicitConversion.cs
+++ NumericConversions/ExplicitConversion.cs
@@ -8,21 +8,57 @@
         }
 
         // TODO #9: Add a static method here with name "FloatToInt" that gets "floatValue" parameter ("float" type) and returns the parameter value that is explicitly converted to "int" type.
+        public static int FloatToInt(float floatValue)
+        {
+            return (int)floatValue;
+        }
 
         // TODO #10: Add a static method here with name "DoubleToInt" that gets "doubleValue" parameter ("double" type) and returns the parameter value that is explicitly converted to "int" type.
+        public static int DoubleToInt(double doubleValue)
+        {
+            return (int)doubleValue;
+        }
 
         // TODO #11: Add a static method here with name "DecimalToInt" that gets "decimalValue" parameter ("decimal" type) and returns the parameter value that is explicitly converted to "int" type.
+        public static int DecimalToInt(decimal decimalValue)
+        {
+            return (int)decimalValue;
+        }
 
         // TODO #12: Add a static method here with name "FloatToLong" that gets "floatValue" parameter ("float" type) and returns the parameter value that is explicitly converted to "long" type.
+        public static long FloatToLong(float floatValue)
+        {
+            return (long)floatValue;
+        }
 
         // TODO #13: Add a static method here with name "DoubleToLong" that gets "doubleValue" parameter ("double" type) and returns the parameter value that is explicitly converted to "long" type.
+        public static long DoubleToLong(double doubleValue)
+        {
+            return (long)doubleValue;
+        }
 
         // TODO #14: Add a static method here with name "DecimalToLong" that gets "decimalValue" parameter ("decimal" type) and returns the parameter value that is explicitly converted to "long" type.
+        public static long DecimalToLong(decimal decimalValue)
+        {
+            return (long)decimalValue;
+        }
 
         // TODO #15: Add a static method here with name "ShortToByte" that gets "shortValue" parameter ("short" type) and returns the parameter value that is explicitly converted to "byte" type.
+        public static byte ShortToByte(short shortValue)
+        {
+            return (byte)shortValue;
+        }
 
         // TODO #16: Add a static method here with name "IntToByte" that gets "intValue" parameter ("int" type) and returns the parameter value that is explicitly converted to "byte" type.
+        public static byte IntToByte(int intValue)
+        {
+            return (byte)intValue;
+        }
 
         // TODO #17: Add a static method here with name "IntToShort" that gets "intValue" parameter ("int" type) and returns the parameter value that is explicitly converted to "short" type.
+        public static short IntToShort(int intValue)
+        {
+            return (short)intValue;
+        }
     }
 }
--- NumericConversions/ImplicitConversion.cs
+++ NumericConversions/ImplicitConversion.cs
@@ -8,19 +8,50 @@
         }
 
         // TODO #1: Add a static method here with name "IntToFloat" that gets "intValue" parameter ("int" type) and returns the parameter value that is implicitly converted to "float" type.
+        public static float IntToFloat(int intValue)
+        {
+            return intValue;
+        }
 
         // TODO #2: Add a static method here with name "IntToDouble" that gets "intValue" parameter ("int" type) and returns the parameter value that is implicitly converted to "double" type.
+        public static double IntToDouble(int intValue)
+        {
+            return intValue;
+        }
 
         // TODO #3: Add a static method here with name "IntToDecimal" that gets "intValue" parameter ("int" type) and returns the parameter value that is implicitly converted to "decimal" type.
+        public static decimal IntToDecimal(int intValue)
+        {
+            return intValue;
+        }
 
         // TODO #4: Add a static method here with name "LongToFloat" that gets "longValue" parameter ("long" type) and returns the parameter value that is implicitly converted to "float" type.
+        public static float LongToFloat(long longValue)
+        {
+            return longValue;
+        }
 
         // TODO #5: Add a static method here with name "LongToDouble" that gets "longValue" parameter ("long" type) and returns the parameter value that is implicitly converted to "double" type.
+        public static float LongToDouble(long longValue)
+        {
+            return longValue;
+        }
 
         // TODO #6: Add a static method here with name "LongToDecimal" that gets "longValue" parameter ("long" type) and returns the parameter value that is implicitly converted to "decimal" type.
+        public static decimal LongToDecimal(long longValue)
+        {
+            return longValue;
+        }
 
         // TODO #7: Add a static method here with name "ByteToShort" that gets "byteValue" parameter ("byte" type) and returns the parameter value that is implicitly converted to "short" type.
-
+        public static short ByteToShort(byte byteValue)
+        {
+            return byteValue;
+        }
         // TODO #8: Add a static method here with name "ByteToInt" that gets "byteValue" parameter ("byte" type) and returns the parameter value that is implicitly converted to "int" type.
+        public static short ByteToInt(byte byteValue)
+        {
+            return byteValue;
+        }
     }
 }
